91e1bb6a38d0878bb71c194caf53355f10c885c0
Bakari Taylor <btaylor31@myseneca.ca>
2025-01-29 18:40:27 -0500

    Lab3a added

---
+++ lab3a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env
+
+# return_text_value() function
+
+def return_text_value():
+    name = 'Terry'
+    greeting = 'Good Morning ' + name
+    return greeting
+
+# return_number_value() function
+
+def return_number_value():
+    num1 = 10
+    num2 = 5
+    num3 = num1 + num2
+    return
+
+# Main Program
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
+    
