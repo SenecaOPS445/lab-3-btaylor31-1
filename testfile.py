002278bfeaf6e23c8809c6803b799b440b3fa900
Bakari Taylor <btaylor31@myseneca.ca>
2025-01-30 15:52:06 -0500

    adding testfile

---
+++ testfile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def hello():
+        print('Hello World')
+        print('Inside a function')
+
+my_stuff = hello()
+print('Stuff return from hello():',my_stuff)
+print('the object my_stuff is of type:',type(my_stuff))
+
+
+               
